2336c65e11c0584b98cd65ffaec14d9a5cf0ecd5
xckouy <xckouy@gmail.com>
2013-04-23 21:57:30 +0800

    555

--- src/TestDemo.java
+++ src/TestDemo.java
@@ -6,6 +6,7 @@ public class TestDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!!!!");
+		System.out.println("Hello world!!!!");
 	}
 
 }
