950c5950c4154e8033db9c64f1423e5c92c2f13b
xckouy <xckouy@gmail.com>
2013-04-23 21:27:51 +0800

    Update SecondDemo.java

--- src/SecondDemo.java
+++ src/SecondDemo.java
@@ -5,7 +5,7 @@ public class SecondDemo {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-                System.out.println("Hello world, hi xc");
+                System.out.println("Hello world, hi xc!");
 	}
 
 }
